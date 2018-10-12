44a8eb5f3305761cea6f144c63643f7ccd268236
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-10-12 17:17:56 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- sample-assignment/m17/TaxiCabNumbers/Solution.java
+++ sample-assignment/m17/TaxiCabNumbers/Solution.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()) {
-			String[] input = sc.nextLine().split(" ");
-			int N = Integer.parseInt(input[0]);
-			int M = Integer.parseInt(input[1]);
-			int n = 1000;
-			MinPQ<Cubesum> pq = new MinPQ<Cubesum>();
-	        	for (int i = 1; i <= n; i++) {
-	            pq.insert(new Cubesum(i, i));
-        	}
+    /**
+     * { function_description }
+     *  Main function.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String[] input = sc.nextLine().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int M = Integer.parseInt(input[1]);
+            int thousand = 1000;
+            int n = thousand;
+            MinPQ<Cubesum> pq = new MinPQ<Cubesum>();
+                for (int i = 1; i <= n; i++) {
+                pq.insert(new Cubesum(i, i));
+            }
             int pair = 1;
             Cubesum prev = new Cubesum(0, 0);
             int pairCount = 0;
@@ -19,20 +28,20 @@ class Solution {
                 if (prev.sum == curr.sum) {
                     pair++;
                     if (pair == M) {
-                    	pairCount = pairCount + 1;
+                        pairCount = pairCount + 1;
                     }
-                    if(pairCount == N) {
-    	            	System.out.println(prev.sum);
-    	            	break;
-                	}
+                    if (pairCount == N) {
+                        System.out.println(prev.sum);
+                        break;
+                    }
                 } else {
                     pair = 1;
                 }
                 prev = curr;
-                if (curr.j < n) { 
-                    pq.insert(new Cubesum(curr.i, curr.j + 1));  
-                } 
-            }
+                if (curr.j < n) {
+                   pq.insert(new Cubesum(curr.i, curr.j + 1));
+                   }
+                               }
         }
     }
 }
