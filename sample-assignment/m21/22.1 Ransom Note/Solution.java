f5170b218a1fe29dc640416d32ec3d42b11c6b7b
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-10-25 20:37:56 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- sample-assignment/m21/22.1 Ransom Note/Solution.java
+++ sample-assignment/m21/22.1 Ransom Note/Solution.java
@@ -1,99 +1,45 @@
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
 /**
  * Class for solution.
  */
 public final class Solution {
     /**
-     * Constructs the object.
+     * client class.
      */
     private Solution() {
-        //constructor.
     }
     /**
      * main function.
      *
-     * @param      args       The arguments
-     *
-     * @throws     Exception  { exception_description }
+     * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);
-        int m = s.nextInt();
-        int n = s.nextInt();
-        String[] str1 = new String[m];
-        for (int i = 0; i < m; i++) {
-            str1[i] = s.next();
-        }
-        String[] str2 = new String[n];
-        for (int i = 0; i < n; i++) {
-            str2[i] = s.next();
-        }
-        if (getRansom(m, n, str1, str2)) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-    }
-    /**
-     * Gets the ransom.
-     *
-     * @param      m         { parameter_description }
-     * @param      n         { parameter_description }
-     * @param      str1  str1
-     * @param      str2    str2
-     *time complexity - O(n).
-     * @return     The ransom.
-     */
-    private static boolean getRansom(final int m, final int n,
-        final String[] str1, final String[] str2) {
-        if (m < n) {
-            return false;
-        }
-        Map<String, Integer> magazineMap = getFrequencyMapFromArray(str1);
-        Map<String, Integer> ransomMap =  getFrequencyMapFromArray(str2);
-        // System.out.println(magazineMap);
-        // System.out.println(ransomMap);
-        for (String key : ransomMap.keySet()) {
-            if (!magazineMap.containsKey(key)) {
-                return false;
-            }
-            if (magazineMap.get(key) < ransomMap.get(key)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    /**
-     * Gets the frequency map from array.
-     * time complexity - O(n).
-     * @param      arr   The arr
-     *
-     * @return     The frequency map from array.
-     */
-    private static Map<String, Integer> getFrequencyMapFromArray(
-        final String[] arr) {
-        Map<String, Integer> map = new HashMap<>();
-        for (String key : arr) {
-            if (map.containsKey(key)) {
-                map.put(key, map.get(key) + 1);
+        Scanner scan = new Scanner(System.in);
+        SeparateChainingHashST<String, Integer> hash =
+        new SeparateChainingHashST<String, Integer>();
+        String integer = scan.nextLine();
+        String[] arr = scan.nextLine().split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (hash.contains(arr[i])) {
+                hash.put(arr[i], hash.get(arr[i]) + 1);
             } else {
-                map.put(key, 1);
+                hash.put(arr[i], 1);
             }
         }
-        return map;
+        String[] search = scan.nextLine().split(" ");
+        for (int i = 0; i < search.length; i++) {
+            if (hash.contains(search[i])) {
+                if (hash.get(search[i]) == 0) {
+                    System.out.print("No");
+                    return;
+                } else {
+                    hash.put(search[i], hash.get(search[i]) - 1);
+                }
+            } else {
+                System.out.print("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
